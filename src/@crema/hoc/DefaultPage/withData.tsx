81f84517c25fee169ec84b6442808b47849ed2b7
Marcelo Cardozo <marcelo.r.cardozo.g@gmail.com>
2021-07-20 22:10:33 -0400

    feat: redirigir a not found si no existe url (#3)

--- src/@crema/hoc/DefaultPage/withData.tsx
+++ src/@crema/hoc/DefaultPage/withData.tsx
@@ -1,25 +1,30 @@
 import React, { useEffect } from 'react';
 import Router, { useRouter } from 'next/router';
 import { useSelector } from 'react-redux';
-import { initialUrl } from '../../../shared/constants/AppConst';
+import { KeycloakInstance } from 'keycloak-js';
+import { useKeycloak } from '@react-keycloak/ssr';
+import { initialUrl } from '@/shared/constants/AppConst';
+import { AppState } from '@/redux/store';
 import Loader from '../../core/Loader';
-import { AppState } from '../../../redux/store';
 
-// eslint-disable-next-line react/display-name
-const withData = (ComposedComponent: any) => (props: any) => {
-  const { user, loading } = useSelector<AppState, AppState['auth']>(
-    ({ auth }) => auth,
-  );
-  const { asPath } = useRouter();
-  const queryParams = asPath.split(`?`)[1];
-  useEffect(() => {
-    if (user) {
-      Router.push(initialUrl + (queryParams ? `?${queryParams}` : ``));
-    }
-  }, [user]);
-  if (loading) return <Loader />;
-  if (user) return <Loader />;
+const withData = (ComposedComponent: any) =>
+  function ComponentWithData(props: any) {
+    const { user, loading } = useSelector<AppState, AppState['auth']>(
+      ({ auth }) => auth,
+    );
+    const { keycloak: { authenticated } = {} } =
+      useKeycloak<KeycloakInstance>();
 
-  return <ComposedComponent {...props} />;
-};
+    const { asPath } = useRouter();
+    const queryParams = asPath.split(`?`)[1];
+    useEffect(() => {
+      if (authenticated) {
+        Router.push(initialUrl + (queryParams ? `?${queryParams}` : ``));
+      }
+    }, [authenticated, queryParams]);
+    if (loading) return <Loader />;
+    if (user) return <Loader />;
+
+    return <ComposedComponent {...props} />;
+  };
 export default withData;
